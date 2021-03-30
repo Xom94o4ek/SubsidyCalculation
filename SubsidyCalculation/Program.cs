6d6540bfadc4d8775736f0e9ba9cc7147f7695f9
Xom94o4ek <irek199707@gmail.com>
2021-03-30 13:12:19 +0300

    Добавьте файлы проекта.

---
+++ SubsidyCalculation/Program.cs
@@ -0,0 +1,91 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace SubsidyCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Volume[] volume = new Volume[5];
+            ///Данные по использованному объему(подходящие)
+            volume[0] = new Volume()
+            {
+                ServiceId = 1,
+                HouseId = 1,
+                Month = new DateTime(2021, 2, 27),
+                Value = 10
+            };
+            ///Данные по использованному объему с некорректным идентификатором услуги
+            volume[1] = new Volume()
+            {
+                ServiceId = 2,
+                HouseId = 1,
+                Month = new DateTime(2021, 2, 27),
+                Value = 11
+            };
+            ///Данные по использованному объему с некорректным идентификатором дома
+            volume[2] = new Volume()
+            {
+                ServiceId = 1,
+                HouseId = 1,
+                Month = new DateTime(2021, 2, 27),
+                Value = 12
+            };
+            ///Данные по использованному объему с некорректным месяцем
+            volume[3] = new Volume()
+            {
+                ServiceId = 1,
+                HouseId = 1,
+                Month = new DateTime(2021, 3, 27),
+                Value = 13
+            };
+            ///Данные по использованному объему с некорректным значением
+            volume[4] = new Volume()
+            {
+                ServiceId = 1,
+                HouseId = 1,
+                Month = new DateTime(2021, 2, 27),
+                Value = -14
+            };
+
+            Tariff[] tariff = new Tariff[2];
+            ///Данные подходящего тарифа
+            tariff[0] = new Tariff()
+            {
+                ServiceId = 1,
+                HouseId = 1,
+                PeriodBegin = new DateTime(2021, 2, 1),
+                PeriodEnd = new DateTime(2021, 2, 28),
+                Value = 100
+            };
+            ///Данные тарифа со значением 0
+            tariff[1] = new Tariff()
+            {
+                ServiceId = 1,
+                HouseId = 1,
+                PeriodBegin = new DateTime(2021, 2, 1),
+                PeriodEnd = new DateTime(2021, 2, 28),
+                Value = 0
+            };
+
+
+            ///Расчет
+            SubsidyCalculation Subsidy = new SubsidyCalculation();
+            Subsidy.OnNotify += dispOnNotify;
+            Subsidy.OnException += dispOnException;
+
+            Charge charge;
+            charge = Subsidy.CalculateSubsidy(volume[0], tariff[0]);
+            Console.WriteLine(charge.Value);
+        }
+        public static void dispOnNotify(object sender, string message)
+        {
+            Console.WriteLine(message);
+        }
+        public static void dispOnException(object sender, Tuple<string, Exception> message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ SubsidyCalculation/SubsidyCalculation.cs
@@ -0,0 +1,68 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace SubsidyCalculation
+{
+    ///<summary>
+    ///Класс расчета субсидии
+    ///</summary>
+    class SubsidyCalculation : ISubsidyCalculation
+    {
+        public event EventHandler<string> OnNotify;
+        public event EventHandler<Tuple<string, Exception>> OnException;
+        /// <summary>
+        /// Вычислении субсидии на дом по указанному объёму и указанному тарифу
+        /// </summary>
+        /// <param name="volumes"></param>
+        /// <param name="tariff"></param>
+        /// <returns>charge</returns>
+        public Charge CalculateSubsidy(Volume volumes, Tariff tariff)
+        {
+            /// Начало проверок соответствия условиями
+            OnNotify?.Invoke(this,"Расчет начат в " + DateTime.Now.ToString("HH:mm:ss dd.MM.yyyy"));
+
+            if (volumes.ServiceId != tariff.ServiceId) NoCorrect("Не совпадают идентификаторы Услуг!");
+
+            if (volumes.HouseId != tariff.HouseId) NoCorrect("Не совпадают идентификаторы Домов!");
+
+            if (volumes.Month.Month < tariff.PeriodBegin.Month || tariff.PeriodEnd.Month < volumes.Month.Month)
+                NoCorrect("Период действия тарифа не распространяется на месяц начисления объема!");
+
+            if (tariff.Value <= 0) NoCorrect("Не допускается использование нулевых или отрицательных значений тарифа!");
+
+            if (volumes.Value < 0) NoCorrect("Значение объема не может быть отрицательным!");
+            /// Конец проверок соответствия условиями
+
+            /// Нахождение значения начисления
+            Charge charge = new Charge();
+            try
+            {
+                charge.ServiceId = volumes.ServiceId;
+                charge.HouseId = volumes.HouseId;
+                charge.Month = volumes.Month;
+                charge.Value = volumes.Value * tariff.Value;
+            }
+            catch (Exception e)
+            {
+                NoCorrect("Произошла ошибка при нахождении значения начисления", e);
+            }
+
+            OnNotify?.Invoke(this, "Расчет успешно закончен в " + DateTime.Now.ToString("HH:mm:ss dd.MM.yyyy"));
+            return charge;
+        }
+        ///<summary>
+        ///Метод, вызываемый при вводе некорректных данных и возникновении ошибки
+        ///</summary>
+        private void NoCorrect(string message)
+        {
+            Exception e = new Exception(message);
+            OnException?.Invoke(this, Tuple.Create(message, e));
+            throw e;
+        }
+        private void NoCorrect(string message, Exception e)
+        {
+            OnException?.Invoke(this, Tuple.Create(message, e));
+            throw e;
+        }
+    }
+}
